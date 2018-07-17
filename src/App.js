f7836eb166f4e145bcc93d8087c3e7d7c0c49a8b
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-07-17 12:55:56 +0900

    jsx記号を加えた

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// jsxをトランスパイルするのに使うために
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+     return (
+         <React.Fragment>
+             <label htmlFor="bar">bar</label>
+             <input type="text" onChange={() => alert('入力されたぜ')}/>
+         </React.Fragment>
+     )
+    }
 }
 
 export default App;
