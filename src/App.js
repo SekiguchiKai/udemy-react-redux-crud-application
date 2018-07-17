1df26cbc6627d89eaf116bd192e9a3b531d6176e
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-07-17 16:00:45 +0900

    propsを追加した

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 // jsxをトランスパイルするのに使うために
 import React, {Component} from 'react';
 
-// class Component
-// class App extends Component {
-//     render() {
-//         return (
-//             <React.Fragment>
-//                 <label htmlFor="bar">bar</label>
-//                 <input type="text" onChange={() => alert('入力されたぜ')}/>
-//             </React.Fragment>
-//         )
-//     }
-// }
-
 // 関数をただ定義しただけで関数Componentになる
 const App = () => {
+    const profiles = [
+        {
+            name: 'Jim',
+            age: 25,
+        },
+        {
+            name: 'Jon',
+            age: 20,
+        },
+        {
+            name: 'NoName'
+        }
+    ];
+
     return (
         <div>
-            <Dog />
-            <Dog />
-            <Dog />
-            <Dog />
-            <Dog />
+            {
+                profiles.map((profile, index) => {
+                    return <User name={profile.name} age={profile.age} key={index}/>
+                })
+            }
         </div>
     )
 };
 
-const Dog = () => {
-    return <div>ワン!</div>
+const User = (props) => {
+    return <div>Hi! Iam {props.name}, and {props.age} years old!</div>
+};
+
+// propsの値が存在しなかった場合のデフォルトの値を設定しておく
+User.defaultProps = {
+    age: 1
 };
 
 export default App;
