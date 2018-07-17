51f1882361eed980f00c25ee04e838c8d959e1a2
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-07-17 15:47:21 +0900

    関数Componentを追加した

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 // jsxをトランスパイルするのに使うために
 import React, {Component} from 'react';
 
-class App extends Component {
-    render() {
-     return (
-         <React.Fragment>
-             <label htmlFor="bar">bar</label>
-             <input type="text" onChange={() => alert('入力されたぜ')}/>
-         </React.Fragment>
-     )
-    }
-}
+// class Component
+// class App extends Component {
+//     render() {
+//         return (
+//             <React.Fragment>
+//                 <label htmlFor="bar">bar</label>
+//                 <input type="text" onChange={() => alert('入力されたぜ')}/>
+//             </React.Fragment>
+//         )
+//     }
+// }
+
+// 関数をただ定義しただけで関数Componentになる
+const App = () => {
+    return (
+        <div>
+            <Dog />
+            <Dog />
+            <Dog />
+            <Dog />
+            <Dog />
+        </div>
+    )
+};
+
+const Dog = () => {
+    return <div>ワン!</div>
+};
 
 export default App;
